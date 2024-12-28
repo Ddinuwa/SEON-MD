49c3e7d91c6c896a2631888053a8316235ab5048
HESHAN-MD <ddinuwa83@gmail.com>
2024-12-28 22:54:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Put the session-id here' : process.env.SESSION_ID,
-GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Put your github username': process.env.GITHUB_USERNAME,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=2I0wVbwS#m_EJ9i_4mVO6TTLg53czuJMMgnsXfbxRGhUGIj56hiI' : process.env.SESSION_ID,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Ddinuwa': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Put your github auth token': process.env.GITHUB_AUTH_TOKEN
 };
